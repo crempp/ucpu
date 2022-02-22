885e70b2607461bbb91b485b269a68fe2af15162
Chad Rempp <crempp@gmail.com>
2022-02-21 20:54:09 -0800

    GPR v1.4, removed old src, deleted a BOM

--- src/build.js
+++
@@ -1,7 +0,0 @@
-import Architecture from "./Architecture.js";
-
-let a = new Architecture();
-a.load('arch.json');
-a.repr();
-a.build();
-a.save();
--- src/compile.js
+++
@@ -1,9 +0,0 @@
-import Compiler from "./Compiler.js";
-
-const DATA_DIR = "../data";
-
-let c = new Compiler(`${DATA_DIR}/test.asm`, 'arch.json');
-c.run();
-
-
-
